25e3ed8fda37103c1a5069ea3df5bd0fa32439b2
James Garrett <jgsibinga@gmail.com>
2017-10-02 14:56:21 -0400

    Overflow Bug Fix
    Declare overflow hidden only on HTML on desktop fixes bug with horizontal scrolling.

--- js/vitality.js
+++ js/vitality.js
@@ -127,3 +127,5 @@ var frames = window.frames;
 // intercom.js
 
 !function(){function t(){var t=a.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://widget.intercom.io/widget/jjqdhhz6";var e=a.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=window,n=e.Intercom;if("function"==typeof n)n("reattach_activator"),n("update",intercomSettings);else{var a=document,c=function(){c.c(arguments)};c.q=[],c.c=function(t){c.q.push(t)},e.Intercom=c,e.attachEvent?e.attachEvent("onload",t):e.addEventListener("load",t,!1)}}();
+
+
